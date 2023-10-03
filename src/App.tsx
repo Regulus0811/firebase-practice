08436f916d9768e876c2aa3fac7df4630a5762d9
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-10-03 17:28:11 +0900

    ✨feat: Implement login feature using Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { auth } from "./firebase";
+import { GoogleAuthProvider, signInWithPopup, User } from "firebase/auth";
+import { useState } from "react";
 
 function App() {
+  const [userData, setUserData] = useState<User | null>(null);
+
+  function handleGoogleLogin() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((data) => {
+        if (data.user) {
+          setUserData(data.user);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>구글 로그인 테스트</h3>
+      <button onClick={handleGoogleLogin}>로그인</button>
+      <h4>로그인하면 아래쪽에 이름이 나타납니다.</h4>
+      <div>
+        {userData && userData.displayName
+          ? "당신의 이름은 : " + userData.displayName
+          : "로그인 버튼을 눌러주세요."}
+      </div>
     </div>
   );
 }
